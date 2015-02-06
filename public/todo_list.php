607e275e1ac77404ec1970cf32793be26b03c34b
Justin Dietert <justin.dietert@gmail.com>
2015-02-05 23:36:32 -0600

    style updates to to-do list project

--- public/todo_list.php
+++ public/todo_list.php
@@ -8,31 +8,33 @@
 <head>
 	<meta charset="UTF-8">
 	<title>To-do List</title>
+	<link href='http://fonts.googleapis.com/css?family=Source+Code+Pro:500|Alegreya+Sans:300,400,400italic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="/css/site.css">
 </head>
 <body>
 	
-	<hr>
+	<div id="container">
+		<!-- list -->
+		<h1 class="fancy">To-do List</h1>
+		<ul class="list">
+			<li>get groceries</li>
+			<li>walk dog</li>
+			<li>eat dinner</li>
+			<li>do homework</li> 
+			<li>sleep</li>
+		</ul>
+		<!-- list end -->
 
-	<!-- list -->
-	<h1>To-do List</h1>
-	<ul>
-		<li>get groceries</li>
-		<li>walk dog</li>
-		<li>eat dinner</li>
-		<li>do homework</li>
-		<li>sleep</li>
-	</ul>
-	<!-- list end -->
+		<hr>
 
-	<hr>
-
-	<!-- form -->
-	<form method="POST" action="/todo_list.php">
-		<label for="todo">Add a to-do:</label>
-		<input type="text" id="todo" name="item" value="" placeholder="Type your to-do here.">
-		<input type="submit" value="Add">
-	</form>
-	<!-- form end -->
+		<!-- form -->
+		<form method="POST" action="/todo_list.php">
+			<label for="todo">Add a to-do:</label>
+			<input type="text" id="todo" name="item" value="" placeholder="Type your to-do here.">
+			<input type="submit" value="Add">
+		</form>
+		<!-- form end -->
+	</div>
 
 </body>
 </html>
